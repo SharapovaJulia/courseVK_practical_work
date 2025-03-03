c675f45fb4e706186b48498a249c88f0cd326219
SharapovaJulia <y12shar@yandex.ru>
2025-03-03 21:52:47 +0500

    third commit

--- practical_work_8.py
+++ practical_work_8.py
@@ -1,13 +1,12 @@
 #Необходимо написать программу, которая будет считывать последовательности измерений. В каждой последовательности нужно выбрать максимальное значение, а в итоге вывести отсортированный по убыванию список этих макс значений, разделенных символом “;”. Во входных данных в первой строке будет задано целое положительное число – сколько записей нужно обработать, причем самих записей может быть больше чем это число, это нужно учесть. Значения в рамках одной записи разделены пробелом, минимальное число значений в записи – 1. Записи разделены переводом строки.
 count_sequence = int(input())
-search_status = True
 max_from_the_sequence = []
-while sequence:= list(input().split()):
-    if count_sequence != 0:
-        for i in range(len(sequence)):
-            sequence[i] = int(sequence[i])
-        max_from_the_sequence.append(max(sequence))
-        count_sequence -= 1
+
+for i in range(count_sequence):
+    sequence = input()
+    sequence = sequence.split()
+    sequence = [int(x) for x in sequence]
+    max_from_the_sequence.append(max(sequence))
+
 max_from_the_sequence.sort(reverse = True)
-max_from_the_sequence = [str(x) for x in max_from_the_sequence]
-print(';'.join(max_from_the_sequence))
+print(';'.join(map(str, max_from_the_sequence)))
