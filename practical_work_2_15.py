0a3f0a4a733a482bd2b3e847f2d99a8146e5f461
SharapovaJulia <y12shar@yandex.ru>
2025-03-16 18:52:17 +0500

    new commit

--- practical_work_2_15.py
+++ practical_work_2_15.py
@@ -4,7 +4,7 @@ start_time = time.time()
 def fibonacci(n):
     numbers = [1, 1]
     for i in range(n-2):
-        numbers.append(numbers[i] + numbers[i + 1])
+        numbers.append(numbers [i] + numbers[i + 1])
     return numbers[-1]
 
 
